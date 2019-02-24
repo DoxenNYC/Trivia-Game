b1802895a06e3963045b965a3e7ba3f36f4a49eb
DoxenNYC <jrdoxen@gmail.com>
2019-02-23 22:48:52 -0600

    new changes

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Trivia-Game
-A New York Knicks themed trivia game that shows only one question until the player answers it or their time runs out.
-If the player selects the correct answer, a correct answer screen will appear along with an image of the correct answer. After a few seconds, the next question will be displayed. Alternatively, if the player selects the wrong answer, the screen will display incorrect, with the correct image as well.
-If the player runs out of time, the screen will that time's up and display the correct answer.
-On the final screen, the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page) will be displayed.
-[link](C:\Users\dreaa\Desktop\Trivia-Game\TriviaGame\index.html)
---
+++ assets/app.js
@@ -0,0 +1,112 @@
+function triviaQuestion(question, options, answer){
+    this.question = question;
+    this.options = options;
+    this.answer = answer;
+  }
+    
+  var Questions = [
+    new triviaQuestion("How many championships have the Knicks won?",["3", "9", "2", "5"],2),
+    
+    new triviaQuestion("What arena do the Knicks play in?",["Madison Square Garden", "Barclays Center", "TD Garden", "Staples Center"],0),
+    
+    new triviaQuestion("Which of these players were NOT drafted by the Knicks?",["Kevin Knox", "Patrick Ewing", "Carmelo Anthony", "Kristaps Porzingis"],2),
+    
+    new triviaQuestion("What number did famous Knick Patrick Ewing wear during his Knicks career?",["53", "23", "3", "33"],3),
+    
+    new triviaQuestion("In 2012, Jeremy Lin had an amazing run who helped the Knicks win many games. What was the nickname for that run?",["Linspiring", "Linton", "Linsanity", "Linwinning"],2),
+    
+    new triviaQuestion("What celebrity is a Knicks super-fan?",["Spike Lee", "Tiger Woods", "Mark Wahlberg", "Tom Brady"],0),
+    
+    new triviaQuestion("Which Knick Player won 2009 Dunk Contest?",["Frank Ntilikina", "Latrell Sprewell", "Nate Robinson", "Carmelo Anthony"],2),
+    
+    new triviaQuestion("Which player was infamously booed by fans for being drafted by the Knicks in the first round?",["Kristaps Porzingis", "Frank Ntilikina", "Patrick Ewing", "Stephon Marbury"],0),
+    
+    new triviaQuestion("What season is the Knicks most recent playoff appearance?",["2007-2008", "2012-2013", "2016-2017", "2017-2018"],1),
+    
+    new triviaQuestion("Who is the owner of the New York Knicks?",["Robert Kraft", "Magic Johnson", "James Dolan", "Tim Hardaway"],2),
+
+  ];
+  
+  var timeLeft = 120;
+    var elem = document.getElementById('timer');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == 0) {
+        clearTimeout(timerId);
+        console.log("Time is up!");
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
+  var currentquestion = 0;
+  var correctAnswers = 0;
+  
+  function setupOptions() {
+    $('#question').html(parseInt(currentquestion) + 1 + ". " + Questions[currentquestion].question);
+    var choices = Questions[currentquestion].options;
+    var formHtml = '';
+    for (var i = 0; i < choices.length; i++) {
+      formHtml += '<div><input type="radio" name="option" value="' + i + '" class="options"><label for="option' + i + '">' + choices[i] + '</label></div><br/>';
+    }
+    $('#form').html(formHtml);
+    $(".options:eq(0)").prop('checked', true);
+  }
+  
+  function checkAns() {
+    if ($("input[name=option]:checked").val() == Questions[currentquestion].answer) {
+      correctAnswers++;
+    }
+  }    
+
+
+  $(document).ready(function(){
+      
+    var $jumbotron = $(".jumbotron");
+    var $start = $("#start");
+    var $progressbar = $("#progressbar");
+    var $next = $("#next");
+    var $result = $("#result");
+    
+      $jumbotron.hide();
+      $start.click(function() {
+          $jumbotron.fadeIn();
+          $(this).hide();
+        });
+  
+      $(function() {
+          $progressbar.progressbar({
+              max: Questions.length-1,			
+              value: 0
+          });
+      });
+  
+      setupOptions();
+  
+      $next.click(function(){
+              event.preventDefault();
+              checkAns();
+              currentquestion++;
+              $(function() {
+                  $progressbar.progressbar({
+                        value: currentquestion
+                  });
+                });
+              if(currentquestion < Questions.length){
+                  setupOptions();
+                  if(currentquestion == Questions.length-1){
+                      $next.html("Submit");
+                      $next.click(function(){
+                          $jumbotron.hide();
+                          $result.html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
+                          $result.fadeIn(1500);
+                      });
+  
+                  }
+                  
+              };
+      });	
+  });
+  
